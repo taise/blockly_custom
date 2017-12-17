b653c65447c1b232d8b3b976ece9eb29d32bf76c
taise <taise515@gmail.com>
2017-12-18 00:40:49 +0900

    workspaceToCode

--- public/index.js
+++ public/index.js
@@ -1,22 +1,14 @@
-Blockly.inject(
+const workspace = Blockly.inject(
   'blocklyDiv',
   {
     toolbox: document.getElementById('toolbox'),
-    maxBlocks: 3,
-    grid: {
-      spacing: 18,
-      length: 3,
-      colour: '#ccc',
-      snap: true,
-    },
     trashcan: true,
-    zoom: {
-      controls: true,
-      wheel: true,
-      startScale: 1.0,
-      maxScale: 3,
-      minScale: 0.3,
-      scaleSpeed: 1.2,
-    },
   },
 );
+function showCode() {
+  Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+  const pre = document.getElementById('jsCode');
+  pre.innerHTML = Blockly.JavaScript.workspaceToCode(workspace);
+}
+
+document.getElementById('showCode').addEventListener('click', showCode, false);
