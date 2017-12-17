4a22fc4c16adec7e202a41ca99a8670ce5045a15
taise <taise515@gmail.com>
2017-12-17 23:11:13 +0900

    Enable eslint & customize Blockly.inject()

--- public/index.js
+++ public/index.js
@@ -1,4 +1,22 @@
-const workspacePlayground = Blockly.inject(
-    'blocklyDiv',
-    { toolbox: document.getElementById('toolbox') }
+Blockly.inject(
+  'blocklyDiv',
+  {
+    toolbox: document.getElementById('toolbox'),
+    maxBlocks: 3,
+    grid: {
+      spacing: 18,
+      length: 3,
+      colour: '#ccc',
+      snap: true,
+    },
+    trashcan: true,
+    zoom: {
+      controls: true,
+      wheel: true,
+      startScale: 1.0,
+      maxScale: 3,
+      minScale: 0.3,
+      scaleSpeed: 1.2,
+    },
+  },
 );
